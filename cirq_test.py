0b1039d926f7ca254bd68baf537ec5997aea66aa
Qbit- <rss3@rice.edu>
2018-07-30 03:28:53 +0300

    Multiple fixes to RE and Cirq calls

--- cirq_test.py
+++ cirq_test.py
@@ -1,47 +1,78 @@
-from src.qOp import  *
+from src.operators import *
 from src.logging import log
-import sys, re
+import sys
+import re
+import numpy as np
 OP = qOperation()
 
-def read_test(filename):
-    log.info("reading file %s"%filename)
-    ops = []
-    with open(filename,"r") as cirquit:
-        idx = 0
-        qbit_count = 0
-        for line in cirquit:
-            if idx==0:
-                qbit_count = int(line)
-                log.info("There are %d qubits in cirquit"%qbit_count)
-            else:
-                m = re.search(r'([0-9]+) [a-z]', line)
-                if not m:
-                    raise Exception("file format error at line %d"%idx)
-                layer_num = int(m.group(1))
 
-                # +1 is for the space between layer idx and gate
-                # -1 is for newline character
-                op_str = line[len(m.group(1))+1 : -1]
-                op = OP.factory(op_str)
-                ops.append(op)
-            idx +=1
-        return ops
+def read_test(filename, max_depth=None):
+    log.info("reading file {}".format(filename))
+    circuit = []
+    circuit_layer = []
+    
+    with open(filename, "r") as fp:
+        qubit_count = int(fp.readline())
+        log.info("There are {:d} qubits in circuit".format(qubit_count))
+        n_ignored_layers = 0
+        current_layer = 0
+
+        for line in fp:
+            m = re.search(r'(?P<layer>[0-9]+) (?=[a-z])', line)
+            if m is None:
+                raise Exception("file format error at line {}".format(idx))
+            # Read circuit layer by layer
+            layer_num = int(m.group('layer'))
+
+            if max_depth is not None and layer_num > max_depth:
+                n_ignored_layers = layer_num - max_depth
+                continue
+
+            if layer_num > current_layer:
+                circuit.append(circuit_layer)
+                circuit_layer = []
+                current_layer = layer_num
+    
+            op_str = line[m.end():]
+            op = OP.factory(op_str)
+            circuit_layer.append(op)
+            
+        circuit.append(circuit_layer) # last layer
+
+        if n_ignored_layers > 0:
+            log.info("Ignored {} layers".format(n_ignored_layers))
+
+    return qubit_count, circuit
+
 
 def main():
-    f = sys.argv[1]
-    ops = read_test(f)
-    cirq_ops = [o.to_cirq(11) for o in ops]
+    # filename = sys.argv[1]
+    filename = 'inst_4x4_10_0.txt'
+    n_qubits, circuit = read_test(filename)
+    side_length = int(np.sqrt(n_qubits))
+    
+    cirq_circuit = cirq.Circuit()
 
-    circuit = cirq.Circuit.from_ops(*cirq_ops)
+    for layer in circuit:
+        cirq_circuit.append(op.to_cirq(side_length) for op in layer)
+        
     print("Circuit:")
-    print(circuit)
+    print(cirq_circuit)
     simulator = cirq.google.XmonSimulator()
 
-    result = simulator.run(circuit, repetitions=20)
+    result = simulator.simulate(cirq_circuit)
     print("Results:")
     print(result)
 
-    print("DONE\n",cirq_ops)
+    print("DONE\n")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
+
+
+with open('inst_4x4_10_0.txt', 'r+') as fp: 
+    line_one = fp.readline()
+    print(f'line_1: {line_one}')
+    for n, line in enumerate(fp):
+        print(f'line {n}: {line}')
---
+++ src/operators.py
@@ -0,0 +1,127 @@
+import numpy as np
+import logging as log
+import re
+import cirq
+
+
+class qOperation:
+    def factory(self, arg):
+        if isinstance(arg, str):
+            return self._create_from_string(arg)
+
+    def _create_from_string(sefl, s):
+        log.debug("creating op from '{}'".format(s))
+        m = re.search(
+            r'(?P<operation>h|t|cz|x_1_2|y_1_2) (?P<qubit1>[0-9]+) ?(?P<qubit2>[0-9]+)?', s)
+        if m is None:
+            raise Exception("file format error in {}".format(s))
+        op_identif = m.group('operation')
+
+        if m.group('qubit2') is not None:
+            q_idx = (int(m.group('qubit1')), int(m.group('qubit2')))
+        else:
+            q_idx = int(m.group('qubit1'))
+
+        if op_identif == 'h':
+            return H(q_idx)
+        if op_identif == 't':
+            return T(q_idx)
+        if op_identif == 'cz':
+            return cZ(q_idx)
+        if op_identif == 'x_1_2':
+            return X_1_2(q_idx)
+        if op_identif == 'y_1_2':
+            return Y_1_2(q_idx)
+
+    def get_grid_idx(self, grid_size):
+        try:
+            return [(i//grid_size, i % grid_size) for i in self.qubit_idx]
+        except TypeError:
+            return [(i//grid_size, i % grid_size) for i in [self.qubit_idx]]
+
+    def to_cirq(self, grid_size):
+        return self.cirq_op(
+            *[cirq.GridQubit(*x) for x in self.get_grid_idx(grid_size)]
+        )
+
+    def __str__(self):
+        return "<{} operator on {}>".format(self.name, self.qubit_idx)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class H(qOperation):
+    matrix = 1/np.sqrt(2) * np.array([[ 1.+0.j,  1.+0.j],
+                                      [ 1.+0.j, -1.+0.j]])
+    name = 'H'
+    cirq_op = cirq.H
+
+    def __init__(self, qubit):
+        if isinstance(qubit, int):
+            self.qubit_idx = qubit
+
+    def apply(self, vec):
+        return np.dot(self.matr, vec)
+
+
+class cZ(qOperation):
+    matrix = np.array([[ 1.+0.j,  0.+0.j,  0.+0.j,  0.+0.j],
+                       [ 0.+0.j,  1.+0.j,  0.+0.j,  0.+0.j],
+                       [ 0.+0.j,  0.+0.j,  1.+0.j,  0.+0.j],
+                       [ 0.+0.j,  0.+0.j,  0.+0.j, -1.+0.j]])
+    cirq_op = cirq.CZ
+    name = 'cZ'
+
+    def __init__(self, qubit):
+        if isinstance(qubit, tuple):
+            self.qubit_idx = qubit
+
+    def apply(self, vec):
+        return np.dot(self.matr, vec)
+
+
+class T(qOperation):
+    matrix = np.array([[1.+0.j,  0.+0.j        ],
+                       [0.+0.j, np.exp(1.j*np.pi/4)]])
+    name = 'T'
+    cirq_op = cirq.T
+
+    def __init__(self, qubit):
+        if isinstance(qubit, int):
+            self.qubit_idx = qubit
+
+    def apply(self, vec):
+        return np.dot(self.matr, vec)
+
+
+class X_1_2(qOperation):
+    matrix = np.array([[0.5+0.5j, 0.5-0.5j],
+                       [0.5-0.5j, 0.5+0.5j]])
+    name = 'X_1_2'
+
+    def cirq_op(s, x): return cirq.X(x)**0.5
+
+    def __init__(self, qubit):
+        if isinstance(qubit, int):
+            self.qubit_idx = qubit
+
+    def apply(self, vec):
+        return np.dot(self.matr, vec)
+
+
+class Y_1_2(qOperation):
+    matrix = np.array([[ 0.5+0.5j, -0.5-0.5j],
+                       [ 0.5+0.5j,  0.5+0.5j]])
+    name = 'Y_1_2'
+
+    def cirq_op(self, x): return cirq.Y(x)**0.5
+
+    def __init__(self, qubit):
+        if isinstance(qubit, int):
+            self.qubit_idx = qubit
+
+    def apply(self, vec):
+        return np.dot(self.matr, vec)
+
+
