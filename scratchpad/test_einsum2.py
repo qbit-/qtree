dc3de85551b58bb1d1aba5b24e712656fab0ff8e
Danil Lykov <lkv97dn@gmail.com>
2020-02-17 03:34:39 -0600

    add flat test

--- scratchpad/test_einsum2.py
+++ scratchpad/test_einsum2.py
@@ -3,86 +3,8 @@ from einsum2 import einsum2
 from threading import Thread
 import psutil
 from time import time, sleep
-from contextlib import contextmanager
+from profilers import timing, cpu_util, proc_count, repeat
 
-def threaded(f, daemon=False):
-    import queue
-
-    def wrapped_f(q, *args, **kwargs):
-        '''this function calls the decorated function and puts the 
-        result in a queue'''
-        ret = f(*args, **kwargs)
-        q.put(ret)
-
-    def wrap(*args, **kwargs):
-        '''this is the function returned from the decorator. It fires off
-        wrapped_f in a new thread and returns the thread object with
-        the result queue attached'''
-
-        q = queue.Queue()
-
-        t = Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
-        t.daemon = daemon
-        t.start()
-        t.result_queue = q
-        return t
-
-    return wrap
-
-def threaded_gen(f):
-    """ makes the function return a generator, 
-    which iterates while thread is running and 
-    yields return value of function at last step.
-    Usage:
-        for res it threaded_gen(f)(*args, **kw):
-            #do stuff here
-        print('Done threaded exec:',res)
-    """
-
-    def wrap(*args, **kwargs):
-        thr_f =  threaded(f)
-        thread = thr_f(*args, **kwargs)
-        while thread.is_alive():
-            yield
-            sleep(.1)
-        thread.join()
-        yield thread.result_queue.get()
-        return
-    return wrap
-
-def proc_count(description='Process count'):
-    def decorator(f):
-        def f_wrapped(*args, **kwargs):
-            thr_gen = threaded_gen(f)
-            pnames = set()
-            for res in thr_gen(*args, **kwargs):
-                names = [proc.name() for proc in psutil.process_iter()]
-                names = {name + str(i) for i, name in enumerate(names) if name=='python'}
-                pnames = pnames | names
-            print(description, len(pnames))
-            return res
-        return f_wrapped
-    return decorator
-
-def cpu_util(description='CPU utilisation:'):
-    def decorator(f):
-        def f_wrapped(*args, **kwargs):
-            thr_gen = threaded_gen(f)
-            utils = []
-            for res in thr_gen(*args, **kwargs):
-                utils.append(psutil.cpu_percent(interval=0))
-            print(description, max(utils))
-            return res
-        return f_wrapped
-    return decorator
-
-
-@contextmanager
-def timing(description: str) -> None:
-    start = time()
-    yield
-    ellapsed_time = time() - start
-    print(f"{description}: {ellapsed_time}")
 
 @proc_count()
 @cpu_util()
@@ -93,6 +15,21 @@ def do_einsum(n_dim=4, dim_size=2, einsum=np.einsum):
     result_idx = list(set(idx1 + idx2))
     T1 = np.random.randn(*shape)
     T2 = np.random.randn(*shape)
+    print(shape)
+
+    res = einsum(T1, idx1, T2, idx2, result_idx)
+    return res
+
+@proc_count()
+@cpu_util()
+def do_einsum_flat(n_dim=4, dim_size=2, einsum=np.einsum):
+    shape = [dim_size] + [dim_size**n_dim]
+    idx1 = [0] + [1]
+    idx2 = [0] + [2]
+    result_idx = list(set(idx1 + idx2))
+    T1 = np.random.randn(*shape)
+    T2 = np.random.randn(*shape)
+    print(shape)
 
     res = einsum(T1, idx1, T2, idx2, result_idx)
     return res
@@ -104,8 +41,11 @@ def profile_einsum(einsum, ndims=range(5, 15), dim_size=2):
                                , dim_size=dim_size
                                , totlen=dim_size**ndim
                               )
+        with timing('Flat: '+msg):
+            do_einsum_flat(n_dim=ndim, dim_size=dim_size, einsum=einsum)
         with timing(msg):
             do_einsum(n_dim=ndim, dim_size=dim_size, einsum=einsum)
+        print('\n')
 
 def profile_both():
     print('numpy')
