bafdf0f7de20f588e665c4ece16e8ef202a2e3c7
Danylo Lykov <danlkv@jlselogin2.ftm.alcf.anl.gov>
2020-03-03 01:12:38 +0000

    no speedup because of data transfer

--- scratchpad/parallel_eliminate_node.py
+++ scratchpad/parallel_eliminate_node.py
@@ -1,17 +1,24 @@
 from multiprocessing import Pool
 import numpy as np
-from profilers import timing, cpu_util, proc_count, repeat
+from profilers import timing, cpu_util, proc_count, repeat, timed
+import profilers
+import os
 
 
+@timed('contract')
 @cpu_util()
 def first_idx_contract(A,B):
     assert A.shape[0] == B.shape[0]
-    return np.einsum('ij, ik -> ijk', A,B)
+    pid = os.getpid()
+    print(pid,'shape',A.shape,B.shape)
+    x =  np.einsum('ij, ik -> ijk', A,B)
+    print('cntr')
+    return x
 
 def unpacked(args):
     return first_idx_contract(*args)
 
-def eliminate(A, B, nproc=2):
+def eliminate(A, B, nproc=16):
     #ops = [A,B]
     N = A.shape[0]
     target_shape = [N]
@@ -21,13 +28,16 @@ def eliminate(A, B, nproc=2):
     A, B = A.reshape((N,-1)), B.reshape((N,-1))
 
     ops = [A, B]
-    chunked = [chunk_slice(x, nproc) for x in ops]
+    #chunked = [chunk_slice(x, nproc) for x in ops]
+    chunk_a = [A for _ in range(nproc)]
+    chunk_b = chunk_slice(B, nproc)
+    chunked = [chunk_a, chunk_b]
 
     pool = Pool(processes=nproc)
     args = zip(*chunked)
     result = pool.map(unpacked, args)
-    return np.concatenate(result)
-
+    print('Done\n')
+    return np.concatenate(result, axis=-1)
 
 
 def chunk_slice(X, nproc):
@@ -35,12 +45,12 @@ def chunk_slice(X, nproc):
     _len = sum(_shape)
     _splits = np.linspace(0, _len, nproc+1, dtype=int)
     print('Splits:', _splits)
-    _splitted = [X[s:e] for s, e in zip(_splits[:-1], _splits[1:])]
+    _splitted = [X[:,s:e] for s, e in zip(_splits[:-1], _splits[1:])]
     return _splitted
 
 def test_par():
-    A = np.random.randn(4, int(10e4))
-    B = np.random.randn(4, 100)
+    A = np.random.randn(4, int(10e1))
+    B = np.random.randn(4, 1000000)
     with timing('Parallel'):
         C = eliminate(A,B)
     print('result shape', C.shape)
