a640d37f5fc1e12c845cfc22fbd930dba31fbb91
Danil Lykov <lkv97dn@gmail.com>
2020-03-05 00:07:22 -0600

    use jupytext to generate py file

---
+++ qaoa/Analyse QAOA cost.py
@@ -0,0 +1,245 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+import qtree 
+import utils
+import utils_qaoa as qaoa
+
+# %load_ext autoreload
+# %autoreload 2
+
+# +
+def get_test_graph(S):
+    G = nx.triangular_lattice_graph(S, S)
+    # remove grid labelling
+    gen = (x for x in range(G.number_of_nodes()))
+    G = nx.relabel_nodes(G, lambda x: next(gen))
+    return G
+
+def get_test_qaoa(S):
+    G = get_test_graph(S)
+    N = G.number_of_nodes()
+    beta, gamma = [np.pi/3], [np.pi/2]
+    qc = qaoa.get_qaoa_circuit(G, beta, gamma)
+    return qc, N
+
+def get_test_expr_graph(S):
+    qc, N = get_test_qaoa(S)
+    graph = qtree.graph_model.circ2graph(N, qc)
+    return graph, N
+
+def get_optimized_expr(S):
+    graph, N = get_test_expr_graph(S)
+    peo, nghs = utils.get_locale_peo(graph, utils.n_neighbors)
+    graph_opt, slice_dict = utils.reorder_graph(graph, peo)
+    return graph_opt, nghs, N
+
+def get_cost_of_task(S):
+    graph_opt, nghs, N = get_optimized_expr(S)
+    mems, flops = qtree.graph_model.cost_estimator(graph_opt)
+    return mems,flops,nghs, N
+
+
+# + active="ipynb"
+# mems, flops, nghs, N = get_cost_of_task(5)
+# utils.plot_cost(mems, flops)
+
+# +
+sizes = np.arange(5,18,1)
+
+def get_est(xs, vals):
+    mem_est = np.polyfit(np.log(xs), np.log(vals), 2)
+    mem_est = np.poly1d(mem_est)
+    est = np.linspace(20,1e3, 100)
+    est = np.log(est)
+    mem_est = mem_est(est)
+    return np.exp(est),  np.exp(mem_est)
+
+
+
+# +
+results = [
+   get_cost_of_task(s) for s in sizes
+]
+def plot_theory(results):
+    sums = [[sum(x) for x in y[:3]] for y in results]
+    memsums, flopsums, nghssums = zip(*sums)
+    ns = list(zip(*results))[3]
+
+    est, mem_est = get_est(ns, memsums)
+    est, flop_est = get_est(ns, flopsums)
+
+
+    plt.plot(ns, memsums, label='mem sum')
+    plt.plot(ns, flopsums, label='flop sum')
+    plt.plot(est, mem_est, '--', label='mem log-log fit')
+    plt.plot(est, flop_est, '--', label='flop log-log fit')
+
+    plt.xlabel('Number of qbits')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.suptitle('QAOA one amplitude simulation cost', fontsize=14)
+    plt.title('MaxCut triangular grids')
+    plt.legend()
+    plt.minorticks_on()
+    plt.grid(which='minor', alpha=0.5, linestyle='-', axis='both')
+
+plot_theory(results)
+
+# + active="ipynb"
+# plt.plot(sizes, ns)
+# -
+
+
+# ## Profiling actual simulation
+
+
+from pyrofiler import mem_util, timed, timing
+from pyrofiler.callbacks import append_to
+
+
+# +
+profile = {}
+
+@mem_util(description='mem', callback=append_to(profile))
+@timed('time', callback=append_to(profile))
+def simulate_circ(circuit, n_qubits):
+    buckets, data_dict, bra_vars, ket_vars = qtree.optimizer.circ2buckets(
+        n_qubits, circuit)
+
+    graph = qtree.graph_model.buckets2graph(
+        buckets,
+        ignore_variables=bra_vars+ket_vars)
+
+    peo, nghs = utils.get_locale_peo(graph, utils.n_neighbors)
+    peo = qtree.graph_model.indices_to_vars(peo, graph)
+    
+    # place bra and ket variables to beginning, so these variables
+    # will be contracted first
+    #peo, treewidth = qtree.graph_model.get_peo(graph)
+
+    peo = ket_vars + bra_vars + peo
+    perm_buckets, perm_dict = qtree.optimizer.reorder_buckets(buckets, peo)
+
+    # extract bra and ket variables from variable list and sort according
+    # to qubit order
+    ket_vars = sorted([perm_dict[idx] for idx in ket_vars], key=str)
+    bra_vars = sorted([perm_dict[idx] for idx in bra_vars], key=str)
+
+    # Take the subtensor corresponding to the initial state
+    initial_state = target_state = 0
+    slice_dict = qtree.utils.slice_from_bits(initial_state, ket_vars)
+    slice_dict.update(
+        qtree.utils.slice_from_bits(target_state, bra_vars)
+    )
+    sliced_buckets = qtree.np_framework.get_sliced_np_buckets(
+        perm_buckets, data_dict, slice_dict)
+    
+    with timing('time_raw', callback=append_to(profile)):
+        result = qtree.optimizer.bucket_elimination(
+            sliced_buckets, qtree.np_framework.process_bucket_np)
+        
+
+
+# +
+for key in profile:
+    profile[key] = []
+    
+profile['N'] = []
+sizes = np.arange(5,25) 
+
+for S in sizes[:]:
+    qc, N = get_test_qaoa(S)
+    profile['N'].append(N)
+    print(N,'qubits')
+    result = simulate_circ(qc, N)
+
+# +
+ns = profile['N']
+
+def to_flop(secs):
+    secs = np.array(secs)
+    secs *= .3*1e12
+    return secs
+def to_nums(mems):
+    mems = np.array(mems)
+    mems = mems/16
+    return mems
+
+mems = to_nums(profile['mem'])
+#mems -= np.min(mems)
+
+
+plt.plot(ns, to_flop(profile['time_raw']), label='time raw')
+plt.plot(ns, to_flop(profile['time']), label='time')
+plt.plot(ns, mems, label='mem')
+plt.legend()
+plt.yscale('log')
+plt.xscale('log')
+# -
+
+results = [
+   get_cost_of_task(s) for s in sizes
+]
+sums = [[sum(x) for x in y[:3]] for y in results]
+memsums, flopsums, nghssums = zip(*sums)
+
+# +
+est, mem_est = get_est(ns, memsums)
+est, flop_est = get_est(ns, flopsums)
+
+fig, axs = plt.subplots(1,2, figsize=(12,5))
+
+def log_log_scale():
+    plt.yscale('log')
+    plt.xscale('log')
+    
+def minorticks():
+    plt.minorticks_on()
+    plt.grid(which='minor', alpha=0.5, linestyle='-', axis='both')
+
+plt.sca(axs[0])
+log_log_scale()
+minorticks()
+plt.plot(ns, memsums, label='theory')
+plt.plot(ns, mems, label='experiment')
+plt.plot(est, mem_est, '--', label='log-log fit')
+plt.legend()
+plt.title('Memory')
+plt.xlabel('Number of qbits')
+
+plt.sca(axs[1])
+log_log_scale()
+minorticks()
+plt.plot(ns, flopsums, label='theory')
+plt.plot(ns, to_flop(profile['time']), label='experiment')
+plt.plot(ns, to_flop(profile['time_raw']), label='experiment raw')
+plt.plot(est, flop_est, '--', label='log-log fit')
+
+plt.xlabel('Number of qbits')
+
+plt.suptitle('QAOA one amplitude simulation cost', fontsize=14)
+plt.title('FLOP')
+plt.legend()
+plt.savefig('qaoa_cost_theory_exp.png')
+# -
+
+
