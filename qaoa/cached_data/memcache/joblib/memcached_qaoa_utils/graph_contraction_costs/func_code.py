330c223304816714c343d5a69d37aa1e1dba7ae3
Danylo Lykov <danlkv@jlselogin1.ftm.alcf.anl.gov>
2020-03-13 23:32:07 +0000

    add random graph costs

--- qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/graph_contraction_costs/func_code.py
+++ qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/graph_contraction_costs/func_code.py
@@ -1,7 +1,7 @@
 # first line: 19
 def graph_contraction_costs(size, peo, p=1, type='grid'):
-    graph_old, N = qaoa_expr_graph(size, p, type=type)
-    peo, nghs = neigh_peo(size, p, type=type)
+    graph_old, N = memory.cache(qaoa_expr_graph)(size, p, type=type)
+    peo, nghs = memory.cache(neigh_peo)(size, p, type=type)
     graph, _ = utils.reorder_graph(graph_old, peo)
     costs = qtree.graph_model.cost_estimator(graph)
     return costs
--- qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/neigh_peo/func_code.py
+++ qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/neigh_peo/func_code.py
@@ -1,5 +1,5 @@
 # first line: 14
 def neigh_peo(size, p=1, type='grid'):
-    graph, N = qaoa_expr_graph(size, p, type=type)
+    graph, N = memory.cache(qaoa_expr_graph)(size, p, type=type)
     peo, nghs = utils.get_locale_peo(graph, utils.n_neighbors)
     return peo, nghs
