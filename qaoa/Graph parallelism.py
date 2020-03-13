c30ad04bdf3ebc202ff559f91e1dee03bb2b5060
Danil Lykov <lkv97dn@gmail.com>
2020-03-13 01:12:43 -0500

    update graph parall py

--- qaoa/Graph parallelism.py
+++ qaoa/Graph parallelism.py
@@ -1,7 +1,7 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
+#     formats: ipynb,py
 #     text_representation:
 #       extension: .py
 #       format_name: light
@@ -15,7 +15,7 @@
 
 # + [markdown] toc=true
 # <h1>Table of Contents<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"><li><span><a href="#Analyse-chopping" data-toc-modified-id="Analyse-chopping-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Analyse chopping</a></span></li></ul></div>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Analyse-chopping" data-toc-modified-id="Analyse-chopping-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Analyse chopping</a></span><ul class="toc-item"><li><span><a href="#Optimal-chops-search-for-task" data-toc-modified-id="Optimal-chops-search-for-task-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>Optimal chops search for task</a></span></li></ul></li></ul></div>
 # -
 
 import sys
@@ -32,7 +32,7 @@ import copy
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
-from utils_mproc import *
+import memcached_qaoa_utils as cached_utils
 sns.set_style('whitegrid')
 # %load_ext autoreload
 # %autoreload 2
@@ -42,14 +42,40 @@ sns.set_style('whitegrid')
 #
 
 # +
-chop_pts = 12
+chop_pts = 3
+def get_chop_idxs(graph, peo, cost, nghs):
+    drop_idx = get_chop_dn_drop(nghs)
+    min_idx = np.argmin(cost[0][:drop_idx])
+    before_min = min_idx - (drop_idx-min_idx)
+    on_plato = 2 * min_idx // 3
+        
+    return min_idx, drop_idx, drop_idx+5
 
-experiment_name = 'skylake_30-38_sum'
+def _cost_before_chop(idxs, cost):
+    mems, floats = cost
+    before_mem = [max(mems[:i]) for i in idxs]
+    return before_mem
+
+def get_chop_dn_drop(nghs):
+    nghs = np.array(nghs)
+    dn = nghs[1:] - nghs[:-1]
+    neg_idx = [i for i, n in enumerate(dn) if n<0]
+    pos_idx = [i for i, n in enumerate(dn) if n>0]
+    drop_idx = neg_idx[0]
+    pos_idx.reverse()
+    before_drop = [i for i in pos_idx if i<drop_idx]
+    return before_drop[0] - 1
 
 
+
+# -
+
+peo, nghs= cached_utils.neigh_peo(23)
+costs = cached_utils.graph_contraction_costs(23, peo)
+utils.plot_cost(*costs)
 
 # +
-sizes = np.arange(30,39)
+sizes = [12, 13]
 
 tasks = [qaoa.get_test_expr_graph(s, 1) for s in sizes]
 graphs =     [g for g, _ in tasks]
@@ -57,19 +83,20 @@ qbit_sizes = [N for _, N in tasks]
 # -
 
 print('Qubit sizes', qbit_sizes)
-pool = Pool(processes=128)
+pool = Pool(processes=1)
 
 
+def n_peo(graph):
+    return utils.get_locale_peo(graph, utils.n_neighbors)
 peos_n = pool.map(n_peo, graphs)
 peos, nghs = zip(*peos_n)
-np.save(f'cached_data/peos_full_{experiment_name}',peos)
 
 # +
+_get_cost = lambda x: qaoa.cost_graph_peo(*x)
 
 with prof.timing('Get full costs naive'):
-    costs = pool.map(get_cost, zip(graphs, peos))
+    costs = pool.map(_get_cost, zip(graphs, peos))
 
-np.save(f'cached_data/costs_full_{experiment_name}',costs)
 
 # +
 chopped_g = [
@@ -81,7 +108,7 @@ chopped_g = [
 costs_before_chop = [
     mem
     for g, peo, cost, ng in tqdm( zip(graphs, peos, costs, nghs) )
-    for mem in cost_before_chop(get_chop_idxs(g, peo, cost, ng), cost)
+    for mem in _cost_before_chop(get_chop_idxs(g, peo, cost, ng), cost)
 ]
 
 # +
@@ -90,7 +117,7 @@ print('contracted graphs', [g.number_of_nodes() for g in chopped_g])
 print('costs before chop', costs_before_chop)
 
 # +
-par_vars = [0,1,2,3,5,7,8,9,10,11 ,12]
+par_vars = [0,1,2,5, 7, 12]
 
 parallelized_g = [
     g
@@ -103,9 +130,9 @@ parallelized_g = [
 print('parallelised graphs', [g.number_of_nodes() for g in parallelized_g])
 
 
-# peos_repeated = zip(*[peos]*len(par_vars)*chop_pts)
-# peos_repeated = [ peo for batch in peos_repeated for peo in batch ]
-
+def n_peo(graph):
+    return utils.get_locale_peo(graph, utils.n_neighbors)
+_pg_peos = tqdm(list(zip(parallelized_g, peos_par)))
 with prof.timing('peos chopped'):
     peos_par_n = pool.map(n_peo, tqdm(parallelized_g))
 peos_par, nghs_par = zip(*peos_par_n)
@@ -122,42 +149,38 @@ def get_qbb_peo(graph):
     return peo, fail
 
 
-# peos_par = [ get_qbb_peo(g) for g in tqdm( parallelized_g ) ]
-# peos_par, fails_qbb = zip(*peos_par)
+peos_par = [ get_qbb_peo(g) for g in tqdm( parallelized_g ) ]
+peos_par, fails_qbb = zip(*peos_par)
 
 tqdm._instances.clear()
 
 
 _pg_peos = tqdm(list(zip(parallelized_g, peos_par)))
 with prof.timing('Costs chopped'):
-    costs_all = pool.map(get_cost, _pg_peos)
+    costs_all = pool.map(_get_cost, _pg_peos)
 
 
+experiment_name = 'small_chops_test'
 
-mems = [sum(m) for m,_ in costs_all ]
-flops = [sum(f) for _,f in costs_all ]
+mems = [max(m) for m,_ in costs_all ]
 
 # +
-_data = np.array(flops).reshape(len(sizes), chop_pts, len(par_vars)) 
-
-print(_data)
-np.save(f'cached_data/{experiment_name}_flops',_data)
-
 _data = np.array(mems).reshape(len(sizes), chop_pts, len(par_vars)) 
+
 print(_data)
-np.save(f'cached_data/{experiment_name}_mems',_data)
+np.save(f'cached_data/{experiment_name}',_data)
 
 
 # -
 
-def trid_plot(x, y, labels, dimspec=(0,1,2)): 
+def trid_plot(x, y, labels, dimspec=(0,1,2), figsize=(15,4)): 
     y = y.transpose(dimspec)
     plot_cnt = y.shape[0]
     line_cnt = y.shape[1]
     def _label_of(dim, idx):
         return labels[dim] + ' ' + str(x[dim][idx])
     
-    fig, axs = plt.subplots(1, plot_cnt, sharey=True, figsize=(15,4))
+    fig, axs = plt.subplots(1, plot_cnt, sharey=True, figsize=figsize)
     try:
         iter(axs)
     except TypeError:
@@ -166,24 +189,59 @@ def trid_plot(x, y, labels, dimspec=(0,1,2)):
         plt.sca(ax)
         plt.title(_label_of(0, i))
         for j in range(line_cnt):
-            plt.plot(x[2], y[i,j], label=_label_of(1, j))
+            color=plt.cm.jet(j/line_cnt)
+            plt.plot(x[2], y[i,j],color=color, label=_label_of(1, j))
             plt.xlabel(labels[2])
             plt.yscale('log')
             plt.minorticks_on()
-            plt.legend()
 
 
+# %pwd
 
+sizes = np.arange(30,39)
+par_vars = [0,1,2,3,5,7,8,9,10,11,12]
+chop_pts = 12
+experiment_name='skylake_sums'
+_data = np.load('./cached_data/skylake_30-38_sum_flops.npy')
+print(_data.shape)
+
+
+# +
 xs = [np.arange(chop_pts), sizes, par_vars]
-trid_plot(xs, _data, ['Chop part', 'Task size', 'Par vars'],(1,0,2))
+#xs = [sizes, par_vars, np.arange(chop_pts)]
+#names = ['Task size', 'Par vars', 'chop part']
+names = ['Chop point', 'Task size', 'par vars']
+
+trid_plot(xs, _data, names ,(1,0,2))
 plt.suptitle('Parallelisation with chopping, naive peo')
 plt.savefig(f'figures/chop_analysis__{experiment_name}.pdf')
 
+# +
 _chopcost = np.array(costs_before_chop).reshape(len(sizes), chop_pts, 1)
 trid_plot([' ', sizes, range(chop_pts)], _chopcost, ['Chop cost', 'Task size', 'Chop part'], (2,0,1))
 
-
-
 print(_chopcost)
+# -
+
+
+# ## Optimal chops search for task
+
+optimal = _data.min(axis=1)
+optimal = optimal[..., np.newaxis]
+print(optimal.shape)
+
+optimal
+xs = [' ', sizes, par_vars]
+trid_plot(xs, optimal, ['Cost scaling, memory sum', 'Task size', 'Par vars'], 
+          (2,0,1), figsize=(6,10))
+plt.savefig(f'figures/skylake_30-38_optimal_{experiment_name}_mems.pdf')
+plt.legend()
+
+optimal
+xs = [' ', sizes, par_vars]
+trid_plot(xs, optimal, ['Cost scaling, flop sum', 'Task size', 'Par vars'],
+          (2,0,1), figsize=(6,10))
+plt.legend()
+plt.savefig(f'figures/skylake_30-38_optimal_{experiment_name}_flops.pdf')
 
 
