8e9ab1fae49f03d07fac9f724ccfbac07e7c660f
Danylo Lykov <danlkv@jlselogin2.ftm.alcf.anl.gov>
2020-03-14 18:08:42 +0000

    update cache

--- qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/neigh_peo/func_code.py
+++ qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/neigh_peo/func_code.py
@@ -1,5 +1,5 @@
 # first line: 14
-def neigh_peo(size, p=1, type='grid'):
-    graph, N = memory.cache(qaoa_expr_graph)(size, p, type=type)
+def neigh_peo(size, p=1, type='grid',**kw):
+    graph, N = memory.cache(qaoa_expr_graph)(size, p, type=type, **kw)
     peo, nghs = utils.get_locale_peo(graph, utils.n_neighbors)
     return peo, nghs
--- qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/qaoa_expr_graph/func_code.py
+++ qaoa/cached_data/memcache/joblib/memcached_qaoa_utils/qaoa_expr_graph/func_code.py
@@ -1,4 +1,4 @@
 # first line: 10
 @memory.cache
-def qaoa_expr_graph(size, p=1, type='grid', seed=42):
-    return qaoa.get_test_expr_graph(size, p, type=type, seed=seed)
+def qaoa_expr_graph(size, p=1, type='grid', seed=42, **kw):
+    return qaoa.get_test_expr_graph(size, p, type=type, seed=seed, **kw)
